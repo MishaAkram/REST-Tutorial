a102808b08a770e77350d1198db1299b76dbd0ec
MishaAkram <mishaakrambaig@gmail.com>
2021-07-29 19:23:18 +0500

    express get post request using mongoose

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const app =express();
-const mongoose =require('mongoose');
+const app = express();
+const mongoose = require('mongoose');
+// const bodyParser =require('body-parser');
+
 require('dotenv/config');
 //middleware
-app.use('/posts',()=>{
-    console.log("this is a middleware running");
-});
+// app.use('/posts',()=>{
+//     console.log("this is a middleware running");
+// });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //Import Routes
-const postsRoute =require('./routes/posts');
-app.use('/posts',postsRoute);
-app.get('/',(req,res)=>{
-    res.send('we are on Home');
-});
+const postsRoute = require('./routes/posts');
+app.use('/posts', postsRoute);
+// app.get('/', (req, res) => {
+//     res.send('we are on Home');
+// });
 //connect to db
 mongoose.connect(
     process.env.DB_CONNECTION,
-    { useUnifiedTopology: true,useNewUrlParser: true },
-()=> console.log("connected to db"))
+    { useUnifiedTopology: true, useNewUrlParser: true },
+    () => console.log("connected to db"))
 //How to we start listening to the server
- app.listen(3000);
+app.listen(3000);
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,45 @@
-const express =require('express');
-const router =express.Router();
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
 //ROUTES
 
-router.get('/',(req,res)=>{
-    res.send('we are on Posts');
+
+//get back all the posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    }
+    catch (err) {
+        res.json({ message: err });
+    }
+});
+
+
+router.get('/:id', async (req, res) => {
+    try{
+          const post=await Post.findById(req.params.id);
+          res.json(post);
+      }
+      catch (err) {
+          res.json({ message: err });
+      }
+});
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    try {
+
+        const savedPost = await post.save()
+        res.json(savedPost);
+    }
+    catch (err) {
+        res.json({ message: err });
+        console.log("error", err);
+    }
 });
 
 module.exports = router;
